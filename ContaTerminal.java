41dd3ae97d3ed2d6fa0803fb5421f8457844fabd
Cesar Dainezi <cesardainezi@gmail.com>
2025-02-04 22:33:59 -0300

    Desafio da trilha de Java Básico, na DIO

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,19 +1,34 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    private int numero;
-    private String agencia, nomeCliente;
-    private float saldo;
-    private Scanner scanner;
+    private static int numeroConta;
+    private static String numeroAgencia, nomeCliente;
+    private static float saldoDisponivel;
+    private static Scanner scanner;
 
-    public ContaTerminal(){
+    public static void main(String[] args) throws Exception {
+        scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da conta!");
+        numeroConta = scanner.nextInt();
+
+        scanner.nextLine(); // Consome '\n'
+
+        System.out.println("Agora, digite o número agência!");
+        numeroAgencia = scanner.nextLine();
+
+        System.out.println("Informe o nome do cliente.");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Finalmente, informe o saldo disponível do cliente.");
+        saldoDisponivel = scanner.nextFloat();
+
+        String mensagem = "Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é "
+                + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoDisponivel
+                + " já está disponível para saque.";
+
+        System.out.println(mensagem);
 
     }
-
-
-
-
-
-
-
 }
